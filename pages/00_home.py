73f5600406bd0a7c80fda2b233ca69a194c067be
s1243001 <s1243001@gm.ncue.edu.tw>
2025-11-26 13:25:36 +0800

    Update 00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -1,1 +1,11 @@
+import solara
 
+
+@solara.component
+def Page():
+    with solara.Column(align="center"):
+        markdown = """
+        Duckdb練習
+        """
+
+        solara.Markdown(markdown)
